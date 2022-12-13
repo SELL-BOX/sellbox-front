99997b791b58197c10f96fe2457118b0043aa301
cola314 <ubj0314@naver.com>
2022-12-13 21:13:10 +0900

    feat: presenter 테스트 페이지 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,123 @@
+import SockJS from 'sockjs-client'
+import { CompatClient, Stomp } from '@stomp/stompjs'
+import { useRef } from 'react'
+
 export default function Home() {
-  return <h1 className="font-bold">Home</h1>
+  const pcConfig = {
+    iceServers: [
+      {
+        urls: 'stun:stun.l.google.com:19302',
+      },
+    ],
+  }
+  const localVideoRef = useRef<HTMLVideoElement>(null)
+  const clientRef = useRef<CompatClient | null>(null)
+
+  const presenterStop = () => {
+    if (clientRef.current) {
+      const client = clientRef.current
+      client.publish({
+        destination: '/stream',
+        body: JSON.stringify({
+          id: 'stop',
+        }),
+      })
+      client.disconnect()
+    }
+  }
+  const presenterStart = () => {
+    const peerConnection = new RTCPeerConnection(pcConfig)
+
+    const socket = new SockJS('http://localhost:8443/ws')
+    const client = Stomp.over(socket)
+    clientRef.current = client
+
+    client.connect({}, async () => {
+      client.subscribe('/user/video/room', (response) => {
+        const res = JSON.parse(response.body)
+        console.log(res)
+        switch (res.id) {
+          case 'presenterResponse': {
+            if (res.response === 'rejected') {
+              alert(res.message)
+            } else if (res.response === 'accepted') {
+              const sdp = res.sdpAnswer
+              peerConnection.setRemoteDescription({
+                type: 'answer',
+                sdp: sdp,
+              })
+            }
+            break
+          }
+          case 'iceCandidate': {
+            peerConnection
+              .addIceCandidate(res.candidate)
+              .catch((err) => console.log(err))
+            break
+          }
+          case 'stopCommunication': {
+            peerConnection.close()
+            client.disconnect()
+          }
+        }
+      })
+
+      // stream 설정
+      const stream = await navigator.mediaDevices.getUserMedia({
+        video: true,
+        audio: true,
+      })
+      if (localVideoRef.current) {
+        localVideoRef.current.srcObject = stream
+      }
+      stream.getTracks().forEach((track) => {
+        peerConnection.addTrack(track, stream)
+      })
+
+      // candidate 전송
+      peerConnection.onicecandidate = (e) => {
+        if (e.candidate) {
+          client.publish({
+            destination: '/stream',
+            body: JSON.stringify({
+              id: 'onIceCandidate',
+              candidate: e.candidate,
+            }),
+          })
+        }
+      }
+
+      // offer 생성 및 전송
+      const offer = await peerConnection.createOffer({
+        offerToReceiveAudio: true,
+        offerToReceiveVideo: true,
+      })
+      peerConnection.setLocalDescription(new RTCSessionDescription(offer))
+      client.publish({
+        destination: '/stream',
+        body: JSON.stringify({
+          id: 'presenter',
+          sdpOffer: offer.sdp,
+        }),
+      })
+    })
+  }
+  return (
+    <>
+      <h1 className="font-bold">Home</h1>
+      <video ref={localVideoRef} autoPlay={true}></video>
+      <button
+        className="block border-2 border-black"
+        onClick={() => presenterStart()}
+      >
+        Presenter Start
+      </button>
+      <button
+        className="block border-2 border-black"
+        onClick={() => presenterStop()}
+      >
+        Stop
+      </button>
+    </>
+  )
 }
